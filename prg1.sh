d909ed7933ab220aec98d3e045e684b4974c0797
Sibangee Jena <153291469+Sibangee1101@users.noreply.github.com>
2023-12-10 19:13:49 +0530

    Add files via upload

---
+++ prg1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "You are testing pre and post increment and decremet operators :-)"
+echo "enter a number - Value of x: "
+read x
+echo "Value after post increament of x by executing x++ is $[x++]"
+echo "Value after pre increament of x by executing ++x is $[++x]"
+echo "Value after post decreament of x by executing x-- is $[x--]"
+echo "Value after pre decreament of x by executing --x is $[--x]"
+#End of script
+
+
